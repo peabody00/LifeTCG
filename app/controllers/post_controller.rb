d5337c11e95e838ec0c960f8e0fc4ff0a0fe913b
Charlie <peabody00@gmail.com>
2021-03-04 13:37:48 -0600

    Posts Routes
    Working on getting CRUD features for user posts.

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,39 +1,90 @@
 class PostController < ApplicationController
   get '/post/post' do
-    @post = Post.all
-    erb :"/post/post"
+    if logged_in?
+      @post = Post.all
+      erb :'/post/post'
+    else
+      redirect to '/login'
+    end
   end
 
-  get '/post/new' do
-    @post = Post.new
-    erb :"/post/new"
+  get '/post/post/new' do
+    if logged_in?
+      erb :'post/new'
+    else
+      redirect to '/login'
+    end
   end
-  
+
   post '/post/post' do
-    @post = Post.create(params)
-    erb :"/post/post"
+    if logged_in?
+      if params[:text] == ""
+        redirect to "/post/post/new"
+      else
+        @post = current_user.posts.build(text: params[:text])
+        if @post.save
+          redirect to "/post/post"
+        else
+          redirect to "/post/post/new"
+        end
+      end
+    else
+      redirect to '/login'
+    end
   end
 
-  get '/post/:id' do
-    @post = Post.find(params[:id])
-    erb :"/post/show"
-  end
-
-  # get '/post/:id/edit' do
-  #   @post = Post.find(params[:id])
-  #   erb :"/post/edit"
+  # get '/tweets/:id' do
+  #   if logged_in?
+  #     @tweet = Tweet.find_by_id(params[:id])
+  #     erb :'tweets/show_tweet'
+  #   else
+  #     redirect to '/login'
+  #   end
   # end
 
-  # patch "/post/:id" do
-  #   @article = Article.find(params[:id])
-  #   @article.update(params[:article])
-  #   redirect to "/post/#{@article.id}"
+  # get '/tweets/:id/edit' do
+  #   if logged_in?
+  #     @tweet = Tweet.find_by_id(params[:id])
+  #     if @tweet && @tweet.user == current_user
+  #       erb :'tweets/edit_tweet'
+  #     else
+  #       redirect to '/tweets'
+  #     end
+  #   else
+  #     redirect to '/login'
+  #   end
   # end
 
-  # delete "/post/:id" do
-  #   Article.destroy(params[:id])
-  #   redirect to "/post"
+  # patch '/tweets/:id' do
+  #   if logged_in?
+  #     if params[:content] == ""
+  #       redirect to "/tweets/#{params[:id]}/edit"
+  #     else
+  #       @tweet = Tweet.find_by_id(params[:id])
+  #       if @tweet && @tweet.user == current_user
+  #         if @tweet.update(content: params[:content])
+  #           redirect to "/tweets/#{@tweet.id}"
+  #         else
+  #           redirect to "/tweets/#{@tweet.id}/edit"
+  #         end
+  #       else
+  #         redirect to '/tweets'
+  #       end
+  #     end
+  #   else
+  #     redirect to '/login'
+  #   end
   # end
 
-
+  # delete '/tweets/:id/delete' do
+  #   if logged_in?
+  #     @tweet = Tweet.find_by_id(params[:id])
+  #     if @tweet && @tweet.user == current_user
+  #       @tweet.delete
+  #     end
+  #     redirect to '/tweets'
+  #   else
+  #     redirect to '/login'
+  #   end
+  # end
 end
--- db/migrate/01_users.rb
+++ db/migrate/01_users.rb
@@ -2,7 +2,7 @@ class Users < ActiveRecord::Migration[4.2]
     def change
         create_table :users do |t|
             t.string :username
-            t.string :email
+            # t.string :email
             t.string :password_digest
             t.integer :life_value
         end
