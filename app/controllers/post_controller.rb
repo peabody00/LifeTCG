93538143b7dc7370aa3dd0b32ea4a51a2e732b66
Charlie <peabody00@gmail.com>
2021-03-04 14:40:18 -0600

    Post Routes Complete
    Routes for Post controller done.

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,5 +1,5 @@
 class PostController < ApplicationController
-  get '/post/post' do
+  get '/post' do
     if logged_in?
       @post = Post.all
       erb :'/post/post'
@@ -8,7 +8,7 @@ class PostController < ApplicationController
     end
   end
 
-  get '/post/post/new' do
+  get '/post/new' do
     if logged_in?
       erb :'post/new'
     else
@@ -16,16 +16,16 @@ class PostController < ApplicationController
     end
   end
 
-  post '/post/post' do
+  post '/post' do
     if logged_in?
       if params[:text] == ""
-        redirect to "/post/post/new"
+        redirect to "/post/new"
       else
         @post = current_user.posts.build(text: params[:text])
         if @post.save
-          redirect to "/post/post"
+          redirect to "/post"
         else
-          redirect to "/post/post/new"
+          redirect to "/post/new"
         end
       end
     else
@@ -33,58 +33,58 @@ class PostController < ApplicationController
     end
   end
 
-  # get '/tweets/:id' do
-  #   if logged_in?
-  #     @tweet = Tweet.find_by_id(params[:id])
-  #     erb :'tweets/show_tweet'
-  #   else
-  #     redirect to '/login'
-  #   end
-  # end
+  get '/post/:id' do
+    if logged_in?
+      @post = Post.find_by_id(params[:id])
+      erb :'post/show'
+    else
+      redirect to '/login'
+    end
+  end
 
-  # get '/tweets/:id/edit' do
-  #   if logged_in?
-  #     @tweet = Tweet.find_by_id(params[:id])
-  #     if @tweet && @tweet.user == current_user
-  #       erb :'tweets/edit_tweet'
-  #     else
-  #       redirect to '/tweets'
-  #     end
-  #   else
-  #     redirect to '/login'
-  #   end
-  # end
+  get '/post/:id/edit' do
+    if logged_in?
+      @post = Post.find_by_id(params[:id])
+      if @post && @post.user == current_user
+        erb :'post/edit'
+      else
+        redirect to '/post'
+      end
+    else
+      redirect to '/login'
+    end
+  end
 
-  # patch '/tweets/:id' do
-  #   if logged_in?
-  #     if params[:content] == ""
-  #       redirect to "/tweets/#{params[:id]}/edit"
-  #     else
-  #       @tweet = Tweet.find_by_id(params[:id])
-  #       if @tweet && @tweet.user == current_user
-  #         if @tweet.update(content: params[:content])
-  #           redirect to "/tweets/#{@tweet.id}"
-  #         else
-  #           redirect to "/tweets/#{@tweet.id}/edit"
-  #         end
-  #       else
-  #         redirect to '/tweets'
-  #       end
-  #     end
-  #   else
-  #     redirect to '/login'
-  #   end
-  # end
+  patch '/post/:id' do
+    if logged_in?
+      if params[:content] == ""
+        redirect to "/post/#{params[:id]}/edit"
+      else
+        @post = Post.find_by_id(params[:id])
+        if @post && @post.user == current_user
+          if @post.update(text: params[:text])
+            redirect to "/post/#{@post.id}"
+          else
+            redirect to "/post/#{@post.id}/edit"
+          end
+        else
+          redirect to '/post'
+        end
+      end
+    else
+      redirect to '/login'
+    end
+  end
 
-  # delete '/tweets/:id/delete' do
-  #   if logged_in?
-  #     @tweet = Tweet.find_by_id(params[:id])
-  #     if @tweet && @tweet.user == current_user
-  #       @tweet.delete
-  #     end
-  #     redirect to '/tweets'
-  #   else
-  #     redirect to '/login'
-  #   end
-  # end
+  delete '/post/:id/delete' do
+    if logged_in?
+      @post = Post.find_by_id(params[:id])
+      if @post && @post.user == current_user
+        @post.delete
+      end
+      redirect to '/post'
+    else
+      redirect to '/login'
+    end
+  end
 end
